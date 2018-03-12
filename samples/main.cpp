06f11d72682f2223acf8b534284df80d72f69e53
John Brochue <john.brochue@ghielectronics.com>
2018-03-12 15:31:34 -0400

    Removed duplicated code block and formatted what's left

--- samples/main.cpp
+++ samples/main.cpp
@@ -3,13 +3,14 @@
 
 BrainPad brain;
 
-uint8_t ScreenBuffer[1024];
+uint8_t buffer[1024];
 
 int main() {
     brain.init();
     brain.serial.printf(" *** BRAINPAD BLINKY TEST ***\r\n");
-    brain.messageBus.listen(DEVICE_ID_BUTTON_A, DEVICE_BUTTON_EVT_CLICK, onClick);
-	TestDisplay();
+    brain.messageBus.listen(DEVICE_ID_BUTTON_A, DEVICE_BUTTON_EVT_CLICK, OnClick);
+    
+    TestDisplay();
 
     while(1) {
         brain.io.led.setDigitalValue(1);
@@ -19,100 +20,55 @@ int main() {
     }
 }
 
-void Pixel(int x, int y, bool set) {
-	if (x >= 0 && x < 128 && y >= 0 && y < 64) {
-		if (set) {
-			ScreenBuffer[(x + (y / 8) * 128) + 1] |= (1 << (y % 8));
-		}
-		else {
-			ScreenBuffer[(x + (y / 8) * 128) + 1] &= (~(1 << (y % 8)));
-		}
-	}
-}
-
-void SetPixel(int x, int y) {
-	Pixel(x, y, true);
-}
-
-void DrawCircle(int x0, int y0, int radius) {
-	int x = 0, y = radius;
-	int dp = 1 - radius;
-	do {
-		if (dp < 0)
-			dp = dp + 2 * (++x) + 3;
-		else
-			dp = dp + 2 * (++x) - 2 * (--y) + 5;
-
-		SetPixel(x0 + x, y0 + y); 
-		SetPixel(x0 - x, y0 + y);
-		SetPixel(x0 + x, y0 - y);
-		SetPixel(x0 - x, y0 - y);
-		SetPixel(x0 + y, y0 + x);
-		SetPixel(x0 - y, y0 + x);
-		SetPixel(x0 + y, y0 - x);
-		SetPixel(x0 - y, y0 - x);
-	} while (x < y);
-
-	SetPixel(x0 + radius, y0);
-	SetPixel(x0, y0 + radius);
-	SetPixel(x0 - radius, y0);
-	SetPixel(x0, y0 - radius);
-}
-
-void TestDisplay() {
-	DrawCircle(20, 20, 10);
-	brain.lcd.InitScreen();
-	brain.lcd.WriteScreenBuffer(ScreenBuffer);
-}
-
-void Pixel(int x, int y, bool set) {
-	if (x >= 0 && x < 128 && y >= 0 && y < 64) {
-		if (set) {
-			ScreenBuffer[(x + (y / 8) * 128) + 1] |= (1 << (y % 8));
-		}
-		else {
-			ScreenBuffer[(x + (y / 8) * 128) + 1] &= (~(1 << (y % 8)));
-		}
-	}
-}
-void SetPixel(int x, int y) {
-	Pixel(x, y, true);
+void SetPixel(int x, int y, bool set) {
+    if (x >= 0 && x < 128 && y >= 0 && y < 64) {
+        int offset = (x + (y / 8) * 128) + 1;
+        int bit = 1 << (y % 8);
+        
+        if (set) {
+            buffer[offset] |= bit;
+        }
+        else {
+            buffer[offset] &= ~bit;
+        }
+    }
 }
 
 void DrawCircle(int x0, int y0, int radius) {
-	int x = 0, y = radius;
-	int dp = 1 - radius;
-	do {
-		if (dp < 0)
-			dp = dp + 2 * (++x) + 3;
-		else
-			dp = dp + 2 * (++x) - 2 * (--y) + 5;
+    int x = 0, y = radius;
+    int dp = 1 - radius;
+    
+    do {
+        if (dp < 0) {
+            dp = dp + 2 * (++x) + 3;
+        }
+        else {
+            dp = dp + 2 * (++x) - 2 * (--y) + 5;
+        }
+            
+        SetPixel(x0 + x, y0 + y, true);
+        SetPixel(x0 - x, y0 + y, true);
+        SetPixel(x0 + x, y0 - y, true);
+        SetPixel(x0 - x, y0 - y, true);
+        SetPixel(x0 + y, y0 + x, true);
+        SetPixel(x0 - y, y0 + x, true);
+        SetPixel(x0 + y, y0 - x, true);
+        SetPixel(x0 - y, y0 - x, true);
+    } while (x < y);
 
-		SetPixel(x0 + x, y0 + y);
-		SetPixel(x0 - x, y0 + y);
-		SetPixel(x0 + x, y0 - y);
-		SetPixel(x0 - x, y0 - y);
-		SetPixel(x0 + y, y0 + x);
-		SetPixel(x0 - y, y0 + x);
-		SetPixel(x0 + y, y0 - x);
-		SetPixel(x0 - y, y0 - x);
-	} while (x < y);
-
-	SetPixel(x0 + radius, y0);
-	SetPixel(x0, y0 + radius);
-	SetPixel(x0 - radius, y0);
-	SetPixel(x0, y0 - radius);
+    SetPixel(x0 + radius, y0, true);
+    SetPixel(x0, y0 + radius, true);
+    SetPixel(x0 - radius, y0, true);
+    SetPixel(x0, y0 - radius, true);
 }
 
 void TestDisplay() {
-	DrawCircle(20, 20, 10);
-	brain.lcd.InitScreen();
-	brain.lcd.WriteScreenBuffer(ScreenBuffer);
+    DrawCircle(20, 20, 10);
+    
+    brain.lcd.InitScreen();
+    brain.lcd.Writebuffer(buffer);
 }
 
-void onClick(Event) {
+void OnClick(Event) {
     brain.serial.printf("CLICK\r\n");
 }
-
-
-
