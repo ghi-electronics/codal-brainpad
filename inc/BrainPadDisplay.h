14e2fa17dbde0ff429ae0c5076a1c6d9ed9d3ab0
Greg Norris <greg.norris@ghielectronics.com>
2018-03-12 12:05:55 -0400

    Fixed BrainPadDisplay.h

--- inc/BrainPadDisplay.h
+++ inc/BrainPadDisplay.h
@@ -1,19 +1,13 @@
 #ifndef BPDISPLAY_H
 #define BPDISPLAY_H
-
 #include "Image.h"
 #include "MbedI2C.h"
 
-#include <string>
+namespace codal {
 
-
-namespace codal
-{
-   class BrainPadDisplay {
-   
+   class BrainPadDisplay {  
   
    uint8_t Data[2];
-   //uint8_t ScreenBuffer[1024];
    int DeviceAddress = 0x78;
    typedef unsigned char byte;
 
@@ -23,51 +17,11 @@ namespace codal
     
     // Initialize the display
     void InitScreen();
+
 	void WriteScreenBuffer(uint8_t * buffer);
 
 	void WriteCommand(int cmd);
 
-	/*
-  
-    // Draw a pixel at given position
-	void SetPixel(int,int);
-
-    void drawBitmap(const uint8_t *bitmap, uint8_t x, uint8_t y, uint8_t width, uint8_t height);
-	 // Draw the border of a circle
-	void DrawCircle(int,int,int);
-
-    // Draw a line from position 0 to position 1
-	void DrawLine(int,int,int,int);
-
-    // Draw the border of a rectangle at the given location
-	void DrawRectangle(int,int,int,int);
-
-    // Fill the rectangle
-	void DrawFilledRectangle(int,int,int,int);
-
-    // set dark pixel
-	void OffPixel(int, int);
-
-    // Display text by the x, y
-	void DrawText(int, int, string, int, int);
-
-	
-    
-    
-    void ClearScreenBuffer();
-
-   protected:
-
-    void Pixel(int,int,bool);
-    
-   // need for text
-	void ClearPartOfScreen(int, int, int, int);
-
-	void DrawSymbol(int, int, char, int, int);
-	*/
-
-
    };
 }
-
 #endif
