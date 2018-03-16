7fa590a032ca664441bfecee223e79b9620edaae
Greg Norris <greg.norris@ghielectronics.com>
2018-03-16 11:35:13 -0400

    Re-added DrawCircle

--- samples/main.cpp
+++ samples/main.cpp
@@ -5,9 +5,9 @@ BrainPad brain;
 
 // ------------Draw pixels the PXT way---------------- //
 
-uint8_t PXTvram[1024];
+uint8_t vram[1024];
 
-void DrawPointInPXTFormat(int x, int y, bool set = true) {
+void DrawPoint(int x, int y, bool set = true) {
     if (x >= 0 && x < 128 && y >= 0 && y < 64) {
         //original code: https://github.com/Microsoft/pxt-common-packages/blob/master/libs/screen/image.cpp#L55
         int offset = y * 16;
@@ -22,11 +22,39 @@ void DrawPointInPXTFormat(int x, int y, bool set = true) {
     }
 }
 
+void DrawCircle(int x0, int y0, int radius) {
+    int x = 0, y = radius;
+    int dp = 1 - radius;
+
+    do {
+        if (dp < 0) {
+            dp = dp + 2 * (++x) + 3;
+        }
+        else {
+            dp = dp + 2 * (++x) - 2 * (--y) + 5;
+        }
+
+        DrawPoint(x0 + x, y0 + y, true);
+        DrawPoint(x0 - x, y0 + y, true);
+        DrawPoint(x0 + x, y0 - y, true);
+        DrawPoint(x0 - x, y0 - y, true);
+        DrawPoint(x0 + y, y0 + x, true);
+        DrawPoint(x0 - y, y0 + x, true);
+        DrawPoint(x0 + y, y0 - x, true);
+        DrawPoint(x0 - y, y0 - x, true);
+    } while (x < y);
+
+    DrawPoint(x0 + radius, y0, true);
+    DrawPoint(x0, y0 + radius, true);
+    DrawPoint(x0 - radius, y0, true);
+    DrawPoint(x0, y0 - radius, true);
+}
+
 void TestDisplay() {
-    DrawCircleInPXTFormat(64, 32, 30);
-    DrawCircleInPXTFormat(64, 32, 20);
-    DrawCircleInPXTFormat(64, 32, 10);
-    DrawPointInPXTFormat(64, 32);
+    DrawCircle(64, 32, 30);
+    DrawCircle(64, 32, 20);
+    DrawCircle(64, 32, 10);
+    DrawPoint(64, 32);
     brain.lcd.writeScreenBuffer(PXTvram);
 }
 // ------------------------------------------ //
