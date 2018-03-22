6743e4e7e3dafd23aa611a496aeaf39c97670918
John Brochue <johnbrochue@outlook.com>
2018-03-22 13:05:57 -0400

    Restored proper license

--- model/BrainPad.cpp
+++ model/BrainPad.cpp
@@ -13,11 +13,15 @@ Software is furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
 THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 */
+
 
 #include "BrainPad.h"
 #include "Timer.h"
@@ -41,7 +45,7 @@ BrainPad::BrainPad() :
     buttonDown(io.buttonDown, ID_PIN_BUTTON_DOWN, DEVICE_BUTTON_ALL_EVENTS, ACTIVE_LOW, PullMode::Up),
     buttonLeft(io.buttonLeft, ID_PIN_BUTTON_LEFT, DEVICE_BUTTON_ALL_EVENTS, ACTIVE_LOW, PullMode::Up),
     buttonRight(io.buttonRight, ID_PIN_BUTTON_RIGHT, DEVICE_BUTTON_ALL_EVENTS, ACTIVE_LOW, PullMode::Up)
-  
+
 {
     // Clear our status
     status = 0;
--- model/BrainPad.h
+++ model/BrainPad.h
@@ -13,6 +13,9 @@ Software is furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
 THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
@@ -43,7 +46,7 @@ DEALINGS IN THE SOFTWARE.
 #include "BrainPadIO.h"
 #include "CodalFiber.h"
 #include "MessageBus.h"
-#include "BrainPadDisplay.h" 
+#include "BrainPadDisplay.h"
 
 // Status flag values
 #define DEVICE_INITIALIZED                    0x01
@@ -68,7 +71,7 @@ namespace codal
             Button                      buttonDown;
             Button                      buttonLeft;
             Button                      buttonRight;
-            
+
             /**
              * Constructor.
              */
