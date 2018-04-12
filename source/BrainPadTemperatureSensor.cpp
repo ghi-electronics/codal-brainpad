38aa6397b6c45653f9be787b860aa67701ad1732
John Brochue <johnbrochue@outlook.com>
2018-04-12 13:12:30 -0400

    Added missing newline

--- source/BrainPadTemperatureSensor.cpp
+++ source/BrainPadTemperatureSensor.cpp
@@ -2,7 +2,8 @@
 
 using namespace codal;
 
-BrainPadTemperatureSensor::BrainPadTemperatureSensor(Pin& pin, uint16_t id) : pin(pin){
+BrainPadTemperatureSensor::BrainPadTemperatureSensor(Pin& pin, uint16_t id) : pin(pin) {
+
 }
 
 int BrainPadTemperatureSensor::readTemperatureCelsius() {
